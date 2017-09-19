7601aff2873166850174fde8672f5c5b4e86d5a4
supermay <wu_jiamei@outlook.com>
2017-09-19 21:21:03 +0200

    Display redux state

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import Photo from './Photo';
 const PhotoGrid = React.createClass({
   render(){
     return(
       <div className="photo-grid">
-        I'm the photo grid.
+        {/* iterate stuff and pass down props *** also set unique key and pay attention to index */}
+        {this.props.posts.map((post,index) => <Photo key={index} index={index} post={post} {...this.props} />)}
       </div>
     )
   }
