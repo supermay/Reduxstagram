8977d2255bafad512902f6879ecff1906bc26563
supermay <wu_jiamei@outlook.com>
2017-09-19 12:28:30 +0200

    Integrate store with router and bug fixed

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -11,15 +11,20 @@ import PhotoGrid from './components/PhotoGrid';
 
 // import react router deps
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+// pass the store
+import store, { history } from './store';
 
 const router = (
-  <Router history={browserHistory}>
-    <Route path='/' components={Main}>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path='/' components={Main}>
       {/* nest routes here */}
       <IndexRoute component={PhotoGrid}></IndexRoute>
       <Route path='/view/:postId' component={Single}></Route>
     </Route>
-  </Router>
+   </Router>
+  </Provider>
 )
 
 // pass this.props.children here => or have the router here
--- client/store.js
+++ client/store.js
@@ -3,14 +3,14 @@ import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 
 // import the root reducer
-import rootReducer rom './reducers/index';
+import rootReducer from './reducers/index';
 
 import comments from './data/comments';
 import posts from './data/posts';
 
 // create an object for the default data
 const defaultState = {
-  posts
+  posts,
   comments
 }
 // store need rootReducer and defaultState
