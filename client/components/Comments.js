5e75ca4e25cb987da8e575a64f9b611149a752c9
supermay <wu_jiamei@outlook.com>
2017-09-22 16:33:51 +0200

    Render Comments on Single Photo Page

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
 const Comments = React.createClass({
+  renderComment(comment, index){
+    return(
+      <div className="comment" key={index}>
+        <p>
+          <strong>{comment.user}</strong>
+          {comment.text}
+          <button className="remove-comment">&times; </button>
+        </p>
+      </div>
+    )
+  },
+
   render(){
     return(
       <div className="comment">
-        <p>I'm a comment.</p>
+        {this.props.postComments.map(this.renderComment)}
       </div>
     )
   }
--- client/components/Single.js
+++ client/components/Single.js
@@ -6,14 +6,17 @@ import Comments from './Comments'
 const Single = React.createClass({
   render(){
     // index of the post. get from url => this.props.params
-    const index = this.props.posts.findIndex((post) => post.code === this.props.params.postId );
+    const { postId } = this.props.params;
+    const index = this.props.posts.findIndex((post) => post.code === postId );
     // get us the post
     const post = this.props.posts[index];
+    // not every post has comments, need to fix it
+    const postComments = this.props.comments[postId] || [];
 
     return(
       <div className="single-photo">
         <Photo index={index} post={post} {...this.props} />
-        <Comments />
+        <Comments postComments={postComments} />
       </div>
     )
   }
