d39ee5d9aa4aa9abdd0833f4329b613f1fc763cb
supermay <wu_jiamei@outlook.com>
2017-09-22 16:14:48 +0200

    Comments Component

---
+++ client/components/Comments.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Comments = React.createClass({
+  render(){
+    return(
+      <div className="comment">
+        <p>I'm a comment.</p>
+      </div>
+    )
+  }
+})
+export default Comments;
